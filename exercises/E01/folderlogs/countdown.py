8c137d19aafb22a37e7fb82db20e7bb386855a76
JoelOscarsson <j_oscarsson@hotmail.com>
2023-06-08 16:52:27 +0200

    need to figure out cron

--- exercises/E01/folderlogs/countdown.py
+++ exercises/E01/folderlogs/countdown.py
@@ -23,18 +23,45 @@ events = np.array([
 # Get the current datetime
 current_datetime = np.datetime64(datetime.now())
 
-# Calculate time differences in seconds
-time_diffs = events['Date'] - current_datetime
-time_left = pd.to_timedelta(time_diffs, unit='s').floor('s').astype(str)
+# Prepare the countdown log string
+countdown_log = "-" * 50 + "\n"
+countdown_log += f"Countdown from {current_datetime}\n"
+countdown_log += "-" * 50 + "\n\n"
 
-df = pd.DataFrame({'Event': events['Event'], 'Date': events['Date'], 'Time left': time_left})
+for event in events:
+    event_name = event['Event']
+    event_date = event['Date']
+    time_left = event_date - current_datetime
+    days_left = time_left.astype('timedelta64[D]')
+    hours_left = (time_left.astype('timedelta64[h]') % np.timedelta64(1, 'D')).astype(int)
+    minutes_left = (time_left.astype('timedelta64[m]') % np.timedelta64(1, 'h')).astype(int)
 
-print(df)
+    countdown_log += f"Countdown to {event_name}: {days_left} days, {hours_left} hours, {minutes_left} minutes\n"
 
-# logging the output
-with open(log_file, 'a') as log:
-    # Redirect the print output to the log file
-    print(events, file=log)
+# Print the countdown log
+print(countdown_log)
 
-print("Structured array printed to the log file successfully.")
+# Write the countdown log to the file
+with open(log_file, 'w') as file:
+    file.write(countdown_log)
+
+
+
+# # -- original
+# # Calculate time differences in seconds
+# time_diffs = events['Date'] - current_datetime
+# time_left = pd.to_timedelta(time_diffs, unit='s').floor('s').astype(str)
+
+# df = pd.DataFrame({'Event': events['Event'], 'Date': events['Date'], 'Time left': time_left})
+
+# print(df)
+
+# #------
+
+# # logging the output
+# with open(log_file, 'a') as log:
+#     # Redirect the print output to the log file
+#     print(events, file=log)
+
+# print("Structured array printed to the log file successfully.")
 
