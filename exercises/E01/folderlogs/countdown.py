3aacbb9c45a829208c319f8a15fdde2920d2f4f3
JoelOscarsson <j_oscarsson@hotmail.com>
2023-06-08 14:21:26 +0200

    E01.1

--- exercises/E01/folderlogs/countdown.py
+++ exercises/E01/folderlogs/countdown.py
@@ -2,22 +2,31 @@ import os
 from datetime import datetime
 import numpy as np
 import timeit
+import pandas as pd
 
+# pathing
+script_path = os.path.abspath(__file__)
+source_file = script_path
+log_file = os.path.join(os.path.dirname(script_path), 'countdown.log')
 
-# Assign values to the structured array directly with datetime64 data type
+# assigning values 
 events = np.array([
-    ('summer_break', np.datetime64('2023-06-09')),
-    ('lia_start', np.datetime64('2023-09-25')),
+    ('summer_break', np.datetime64('2023-06-09T15:00')),
+    ('lia_start', np.datetime64('2023-09-25T08:00')),
     ('christmas', np.datetime64('2023-12-24')),
     ('bellas_birthday', np.datetime64('2023-12-07')),
     ('new_year', np.datetime64('2024-01-01')),
-    ('graduation_party', np.datetime64('2024-06-09'))
-], dtype=[('name', 'U20'), ('date', 'datetime64[D]')])
+    ('graduation_party', np.datetime64('2024-06-09T16:30'))
+], dtype=[('Event', 'U20'), ('Date', 'datetime64[s]')])
 
-# Print the structured array
-print(events)
+# creating a dataframe for the output
+df= pd.DataFrame(events)
+print(df)
 
+# logging the output
+with open(log_file, 'a') as log:
+    # Redirect the print output to the log file
+    print(events, file=log)
 
-countdown_py_path = os.path.abspath(__file__)
-countdown_dir = os.path.dirname(countdown_py_path)
-log_filepath = os.path.join(countdown_dir, "folderlogs", "countdown.log")
+print("Structured array printed to the log file successfully.")
+
